408369a5d2f0f2e0f180ac87a7faf556d5f86279
frarosset <37275581+frarosset@users.noreply.github.com>
2025-05-22 10:19:27 +0200

    feat: initialize server to listen on a given port and returning the html index page

---
+++ server.js
@@ -0,0 +1,23 @@
+const http = require("node:http");
+const fs = require("node:fs");
+
+port = 80;
+
+http
+  .createServer((req, res) => {
+    console.log("Requesting file:", req.url);
+
+    switch (req.url) {
+      case "/":
+        // Load the index page and return it
+        fs.readFile("./public/index.html", (err, data) => {
+          if (err) {
+            throw err;
+          }
+
+          res.writeHead(200, { "Content-Type": "text/html" });
+          res.end(data);
+        });
+    }
+  })
+  .listen(port, () => console.log(`Server listening on port ${port}`));
