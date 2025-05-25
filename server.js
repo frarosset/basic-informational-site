b8002bf19b6d33a36ee342225f1d1d81687c437f
frarosset <37275581+frarosset@users.noreply.github.com>
2025-05-25 15:08:32 +0200

    refactor: refactor/simplify server.js
    Now the server can process any get request.
    
    If req.url is "/", "/public/index.html" is returned.
    
    If req.url is without extension, it is assumed the extension is ".html"
    and "/public/[req.url].html" is returned.
    
    Else, "/public/[req.url]" is returned.

--- server.js
+++ server.js
@@ -1,51 +1,48 @@
 const http = require("node:http");
 const fs = require("node:fs");
+const path = require("node:path");
+const contentTypes = require("./assets/contentTypes.json");
 
 const port = 80;
-const publicUrl = "./public";
+const publicUrl = "./public"; // relative to the working directory
+const baseUrl = "/index.html"; // page to load at url "/"
 
 http
   .createServer((req, res) => {
     console.log("Requesting file:", req.url);
-    let filepath = req.url;
-    let returnCode = 200;
-    let contentType = "text/plain";
 
-    switch (req.url) {
-      case "/":
-        filepath = "/index.html";
-        contentType = "text/html";
-        break;
-      case "/about":
-        filepath = "/about.html";
-        contentType = "text/html";
-        break;
-      case "/styles/styles.css":
-        contentType = "text/css";
-        break;
-      case "/js/loadDailyData.js":
-        contentType = "text/javascript";
-        break;
-      case "/svg/astro.svg":
-        contentType = "image/svg+xml";
-        break;
-      case "/favicon.ico":
-        // Ignore favicon icon request
-        console.log("Ignoring favicon request");
+    // Ignore favicon icon request
+    if (req.url == "/favicon.ico") {
+      console.log("Ignoring favicon request");
 
-        res.writeHead(204); // No Content
-        res.end();
-        return;
+      res.writeHead(204); // No Content
+      res.end();
+      return;
     }
 
-    // Load the relative path and return it
-    fs.readFile(publicUrl + filepath, (err, data) => {
+    const { filePath, contentType } = getInfoOfFileToGet(req.url);
+
+    // Load the file at the path (relative the the working directory) and return it
+    fs.readFile(filePath, (err, data) => {
       if (err) {
         throw err;
       }
 
-      res.writeHead(returnCode, { "Content-Type": contentType });
+      res.writeHead(200, { "Content-Type": contentType }); // OK
       res.end(data);
     });
   })
   .listen(port, () => console.log(`Server listening on port ${port}`));
+
+function getInfoOfFileToGet(reqUrl) {
+  const url = reqUrl === "/" ? baseUrl : reqUrl;
+
+  const urlExt = path.extname(url);
+  const fileExt = urlExt || ".html"; // default extension is .html
+
+  const filePath = urlExt == "" ? url + fileExt : url;
+
+  const contentType = contentTypes[fileExt];
+
+  return { filePath: publicUrl + filePath, contentType };
+}
