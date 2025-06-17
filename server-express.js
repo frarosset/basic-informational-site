41e435ab05c03093d96957b4dec1e2b067aef459
frarosset <37275581+frarosset@users.noreply.github.com>
2025-06-17 12:08:05 +0200

    feat: rewrite server using Express

---
+++ server-express.js
@@ -0,0 +1,56 @@
+const express = require("express");
+const path = require("node:path");
+require("dotenv").config();
+
+const app = express();
+
+const port = process.env.PORT || 8080;
+const publicUrl = process.env.PUBLIC_URL || "./public"; // relative to the working directory
+const indexUrl = process.env.BASE_URL || "/index.html"; // page to load at url "/"
+
+// Serve static files (HTML, CSS, JS)
+const absPublicUrl = path.join(__dirname, publicUrl);
+app.use(
+  express.static(absPublicUrl, { extensions: ["html"], index: indexUrl })
+);
+
+// Ignore favicon icon / ... request
+app.get(
+  ["/favicon.ico", "/.well-known/appspecific/com.chrome.devtools.json"],
+  (req, res) => {
+    console.log(`Ignoring request of ${req.path}`);
+    res.sendStatus(204); // No Content
+  }
+);
+
+// catch-all route throwing an ENOENT error (404 error)
+// see https://stackoverflow.com/questions/34696848/correct-way-to-handle-404-and-500-errors-in-express
+app.use((req, res, next) => {
+  const err = new Error("File not found");
+  err.code = "ENOENT";
+  throw err;
+});
+
+// Error handling (404, 500)
+// see https://stackoverflow.com/questions/34696848/correct-way-to-handle-404-and-500-errors-in-express
+app.use((err, req, res, next) => {
+  console.log(err);
+
+  // load error page (and handle possible errors on reading files)
+  const { filePath, returnCode } = getInfoOfErrorFile(err);
+
+  res
+    .status(returnCode)
+    .sendFile(path.join(absPublicUrl, filePath), (errOnErr) => {
+      if (errOnErr) res.sendStatus(returnCode);
+    });
+});
+
+app.listen(port, () => console.log(`Server listening on port ${port}`));
+
+function getInfoOfErrorFile(err) {
+  const returnCode = err.code === "ENOENT" ? 404 : 500;
+  const filePath = `/${returnCode}.html`;
+
+  return { filePath, returnCode };
+}
